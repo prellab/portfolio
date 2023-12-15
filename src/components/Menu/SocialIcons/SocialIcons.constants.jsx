22c21bfee913171065082ef485c3af83f3657030
kittystardev <kittystardev@gmail.com>
2023-12-15 16:45:50 -0500

    update

---
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -0,0 +1,7 @@
+// import cv from "./CV.pdf";
+
+export const socialLinks = [
+  { name: "Github", link: "https://github.com/kittystardev" },
+  // {name: "Resume", link: cv},
+  { name: "Mail", link: "mailto:larrywaynehayes@gmail.com" },
+];
