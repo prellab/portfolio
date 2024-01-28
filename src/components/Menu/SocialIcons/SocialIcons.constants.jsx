d005b11affcd3d3dca96b8014f38676fc1f4c53c
kittystardev <kittystardev@gmail.com>
2024-01-28 07:08:58 -0500

    update

--- src/components/Menu/SocialIcons/SocialIcons.constants.jsx
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -1,7 +1,7 @@
 // import cv from "./CV.pdf";
 
 export const socialLinks = [
-  { name: "Github", link: "https://github.com/ihelpbpov040" },
+  { name: "Github", link: "https://github.com/cartoonweber" },
   // {name: "Resume", link: cv},
   { name: "Mail", link: "mailto:ihelpbpov040@gmail.com" },
 ];
